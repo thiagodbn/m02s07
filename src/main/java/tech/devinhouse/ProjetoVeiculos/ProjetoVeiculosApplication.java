22d5c0e3d1e540e3cca4f53333bb564d7550565f
Noronha IntelliJ IDEA <thiagodbn@gmail.com>
2023-12-24 18:23:45 -0300

    inicio mini projeto veiculos

---
+++ src/main/java/tech/devinhouse/ProjetoVeiculos/ProjetoVeiculosApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.ProjetoVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devinhouse/ProjetoVeiculos/ProjetoVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.ProjetoVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
